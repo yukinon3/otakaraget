a1d7515f2a91bf7bb1d6652dd9477ecf2a61e594
yukinon3 <kyeo@outlook.jp>
2015-06-27 17:29:48 +0900

    移動

---
+++ otakaraget/otakaraget/Assets/script/action.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class action : MonoBehaviour {
+
+	float speed = 5.0f;
+
+	controller.Move (new Vector3 (5, 2, 5));
+	//float x = 5;
+	//float y = 2;
+	//float z = 5;
+
+	// Use this for initialization
+	void Start () {
+
+		Controller = GetComponent<CharacterController> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		CameraAxisControl ();
+		attachMove ();
+		attachRotation ();
+	}
+
+	void NormalControl(){
+		if (Controller.isGrounded) {
+			moveDirection = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
+			moveDirection = transform.TransformDirection (moveDirection);
+			moveDirection *= speed;
+		}
+	}
+
+	void CameraAxisControl() {
+		if (ControllerColliderHit.isGrounded) {
+			Vector3 foward = Camera.mainCamera.transform.TransformDirection (Vector3.forward);
+			Vector3 right = Camera.mainCamera.transform.TransformDirection (Vector3.right);
+
+			moveDirection = Input.GetAxis ("Horizontal") * right + Input.GetAxis ("Vertical") * foward;
+			moveDirection *= speed;
+		}
+	}
+
+	void attachRotation() {
+		var moveDirectionYzero = -moveDirection;
+		moveDirectionYzero.y = 0;
+
+		if (moveDirectionYzero.sqrMagnitude > 0.001) {
+		    
+			float    step = rotateSpeed*Time.deltaTime;
+			Vector3 newDir = Vector3.RotateTowards(transform.forward,moveDirectionYzero,step,0f);
+			
+			transform.rotation = Quaternion.LookRotation(newDir);
+		}
+			
+	}
+}
+		
---
+++ otakaraget/otakaraget/Assets/script/jump.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class jump : MonoBehaviour {
+
+	private int restJumps = 2;
+	public float power = 500f;
+	private Rigidbody _rigidbody;
+		
+	// Use this for initialization
+	void Start () {
+		_rigidbody = this.GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			if (restJumps > 0) {
+				this._rigidbody.AddForce (Vector3.up * power);
+				restJumps--;
+				Debug.Log (restJumps);
+			}
+		}
+	}
+
+		void OnCollisionEnter (Collision collision) {
+			if (collision.gameObject.tag == "Floor" )
+			{
+						restJumps = 2;
+					}
+		Debug.Log (restJumps);
+				}
+			
+}
+
+
+
